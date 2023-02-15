41e6b736e708a3e2429aac413f05920a47de9f32
unknown <prajapatabhishek9326@gmail.com>
2023-02-16 00:52:31 +0530

    final commmit of today

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-import 'authWelcome.dart';
+import 'package:plantshop/screens/authWelcome.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
